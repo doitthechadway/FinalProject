d0708a454ba02fc3abdb3a01a0966509defb858d
doitthechadway <waychad1@gmail.com>
2020-11-16 11:29:04 -0600

    Initial commit - New ASP.NET solution

---
+++ Auditions.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Auditions.UI.MVC.MvcApplication" Language="C#" %>
---
+++ Auditions.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Auditions.UI.MVC.Startup))]
+namespace Auditions.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
